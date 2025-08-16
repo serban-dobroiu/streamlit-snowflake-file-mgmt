5735ea35f50c6fd98b7b837130a7ec9a8a2cc3aa
dobroiu@hpe.com <dobroiu@hpe.com>
2025-08-16 09:43:08 +0300

    update DEPLOYMENT_DATE data type to DATE

--- snowflake/data.sql
+++ snowflake/data.sql
@@ -1,20 +1,20 @@
-CREATE TABLE IF NOT EXISTS DEV_SOME_DB.BMT.STG_ACCOUNT_ROSTER (
+CREATE OR REPLACE TABLE DEV_SOME_DB.BMT.STG_ACCOUNT_ROSTER (
     ACCOUNT_ID VARCHAR(10) NOT NULL COMMENT 'account identifier' 
     , PROGRAM_ID VARCHAR(18) NOT NULL COMMENT 'program identifier'
     , POTENTIAL NUMBER(9,2) COMMENT 'dollar potential'
     , VETTED VARCHAR(20) NOT NULL COMMENT 'account and program vetting status'
     , REMOVED BOOLEAN NOT NULL COMMENT 'logical delete'
-    , DEPLOYMENT_DATE TIMESTAMP_NTZ(3) NOT NULL /*DEFAULT '999-12-31 23:59:59.999'::timestamp_ntz(3)*/ COMMENT 'date of account and program deployment'
+    , DEPLOYMENT_DATE DATE NOT NULL COMMENT 'date of account and program deployment'
     , LOAD_TS TIMESTAMP_NTZ(3) DEFAULT SYSDATE()::TIMESTAMP_NTZ(3) COMMENT 'load timestamp'
 );
 
-CREATE TABLE IF NOT EXISTS DEV_SOME_DB.BMT.ACCOUNT_ROSTER (
+CREATE OR REPLACE TABLE DEV_SOME_DB.BMT.ACCOUNT_ROSTER (
     ACCOUNT_ID VARCHAR(10) NOT NULL COMMENT 'account identifier' 
     , PROGRAM_ID VARCHAR(18) NOT NULL COMMENT 'program identifier'
     , POTENTIAL NUMBER(9,2) COMMENT 'dollar potential'
     , VETTED VARCHAR(20) NOT NULL COMMENT 'account and program vetting status'
     , REMOVED BOOLEAN COMMENT 'logical delete'
-    , DEPLOYMENT_DATE TIMESTAMP_NTZ(3) NOT NULL /*DEFAULT '999-12-31 23:59:59.999'::timestamp_ntz(3)*/ COMMENT 'date of account and program deployment'
+    , DEPLOYMENT_DATE DATE NOT NULL COMMENT 'date of account and program deployment'
     , VALID_FROM TIMESTAMP_NTZ(3) COMMENT 'record validity start'
     , VALID_TO TIMESTAMP_NTZ(3) COMMENT 'record validity end'
     , STAGING_LOAD_TS TIMESTAMP_NTZ(3) COMMENT 'record staging timestamp'
