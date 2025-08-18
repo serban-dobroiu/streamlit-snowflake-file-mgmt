8e09f34e2878e5bf965a0431c3860165deece959
dobroiu@hpe.com <dobroiu@hpe.com>
2025-08-18 15:21:30 +0300

    build header from corresponding model fields

--- other_utils.py
+++ other_utils.py
@@ -1,12 +1,14 @@
-from openpyxl import Workbook
+import csv
+from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl.utils import get_column_letter
 from openpyxl.comments import Comment
 from io import BytesIO
 
+from models.account_roster import RosterEntryUpload
 from snowflake_utils import get_table_columns
 
-def create_excel_template(snowflake_session, table_name, worksheet_title: str):
+def create_excel_template(snowflake_session, table_name, worksheet_title: str): # TODO: this needs to generalize
     """
     Need to document this fully
     """
@@ -15,7 +17,9 @@ def create_excel_template(snowflake_session, table_name, worksheet_title: str):
     ws = wb.active
 
     ws.title = worksheet_title
-    headers = get_table_columns(snowflake_session=snowflake_session, table_name = table_name)
+    #TODO: need a general way to exclude fields? Maybe create model?
+    # headers = get_table_columns(snowflake_session=snowflake_session, table_name = table_name)
+    headers = list(RosterEntryUpload.model_fields.keys())
     dummy_data = [''] * len(headers)
 
     data = [headers, dummy_data]
@@ -40,6 +44,26 @@ def create_excel_template(snowflake_session, table_name, worksheet_title: str):
     wb.save(io)
 
     return io
+
+
+def convert_excel_to_csv(uploaded_file: BytesIO):
+
+    wb = load_workbook(uploaded_file, data_only=True, read_only=True)
+
+    ws = wb.active
+
+    headers = [cell.value for cell in next(ws.rows)]
+
+    with open("Account_Roster.csv", mode = "w") as file_handle:
+        csv_writer = csv.DictWriter(file_handle, fieldnames=headers)
+        csv_writer.writeheader()
+        for row in ws.iter_rows(min_row=2):
+            values = [cell.value for cell in row]
+            data = dict(zip(headers, values))
+            valid_record = RosterEntryUpload(**data)
+            print(valid_record)
+            csv_writer.writerow(valid_record.model_dump())
+    return file_handle
 
 # wb = Workbook()
 
